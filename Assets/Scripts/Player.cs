248031f3cf2b26094d92b91e854374da12b0eb9f
krutozavr1 <ivannsharipkov@gmail.com>
2022-11-22 14:57:41 +0300

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    Vector2 dir;
+    float str;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void SetDirictionOfKick()
+    {
+
+    }
+
+    private void SetStrenghtOfKick()
+    {
+
+    }
+}
